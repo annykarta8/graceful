a81a7eecbee86e42d0c84c3b43b475599cfe13bf
Annie Lu <annykarta8@gmail.com>
2021-07-28 20:52:36 +0800

    依照登入者去導入不同的畫面

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,9 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/redirects', [HomeController::class, 'redirects']);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
