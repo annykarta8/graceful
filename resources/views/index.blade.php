c4119ebc4c4c5738b5ea52c8b3e35339a663bad4
Annie Lu <annykarta8@gmail.com>
2021-07-28 15:09:14 +0800

    設計首頁的版面與樣式

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>首頁</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="title">
+        <div class="left">
+            <img src="https://cdn4.iconfinder.com/data/icons/childhood-objects-2/128/_leaf_plant_hand_drawn_abstract-117-2-256.png"
+                width="70" height="70" />
+        </div>
+        <div class="right">
+            <div class="item">Index</div>
+            <div class="item">Shop</div>
+            <div class="item">About</div>
+            <div class="item">Connect</div>
+            <div class="bar"><img src="image/bar.png" width="50" height="50" /></div>
+        </div>
+    </div>
+    <div class="welcome">
+        <div class="text">Welcome</div>
+    </div>
+    <div class="section">Top</div>
+    <div class="box">
+        <div class="list">
+            <div class="content">
+                <img src="image/pic1.jpg" />
+            </div>
+            <div class="content">
+                <img src="image/pic2.jpg" />
+            </div>
+        </div>
+        <div class="list">
+            <div class="content">
+                <img src="image/pic3.jpg" />
+            </div>
+            <div class="content">
+                <img src="image/pic4.jpg" />
+            </div>
+        </div>
+    </div>
+    <div class="foot">
+        <div class="calltoaction">call to Action</div>
+    </div>
+</body>
+
+</html>
