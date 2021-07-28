47267b6443c6df15ec60340f9f03c2aaccf1b1da
Annie Lu <annykarta8@gmail.com>
2021-07-28 16:03:31 +0800

    加入jetstream的登入套件

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>首頁</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -19,6 +20,22 @@
             <div class="item">Shop</div>
             <div class="item">About</div>
             <div class="item">Connect</div>
+            <div class="item">
+                @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                    @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+
+                    @endif
+                    @endauth
+                </div>
+                @endif
+            </div>
             <div class="bar"><img src="image/bar.png" width="50" height="50" /></div>
         </div>
     </div>
